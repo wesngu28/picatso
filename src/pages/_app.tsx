a9ff8e29e9d622075c3cb0e302681bb83675b010
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-02-26 01:19:00 -0800

    meta, favicon and analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import { type AppType } from "next/app";
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import Script from "next/script";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <UserProvider>
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
+      <noscript>{/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
       <Component {...pageProps} />
     </UserProvider>
   );
